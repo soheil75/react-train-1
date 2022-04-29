421b188b1ec99c071c54ca7348234a33f0385a5a
soheil75 <soheilabdollahi1375@yahoo.com>
2022-04-29 18:17:38 +0430

    fragment add to page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,Fragment } from "react";
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
@@ -12,10 +12,14 @@ function App() {
   };
 
   return (
-    <div>
+    <>
+    {/* <React.Fragment> */}
+    {/* <Fragm/ent> */}
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+      {/* </Fragment> */}
+      {/* </React.Fragment> */}
+    </>
   );
 }
 
